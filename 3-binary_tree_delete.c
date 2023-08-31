fcfa25328388c496dc5d52e98dc5df3aa31383f5
Reem-Kamal-Ghoniem <reemkamal111@gmail.com>
2023-08-30 18:42:31 -0700

    checking leaf

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -9,7 +9,7 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return;
-	while (tree != NULL)
+	else
 	{
 		binary_tree_delete(tree->left);
 		binary_tree_delete(tree->right);
--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -6,9 +6,7 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL)
-		return (0);
-	if (node->right == NULL && node->left)
+	if (node->right == NULL && node->left && node != NULL)
 		return (1);
 	else
 		return (0);
