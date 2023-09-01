85760cfee27dccc9d4787022b5be4d4016050449
Fadyy22 <fadyalaa441@gmail.com>
2023-09-02 01:03:57 +0300

    find uncle

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - finds the uncle of a node
+ *
+ * @node: pointer to the node to find the uncle
+ *
+ * Return: pointer to the uncle node
+ * If node is NULL, return NULL
+ * If node has no uncle, return NULL
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL)
+		return (NULL);
+
+	if (node->parent->parent)
+	{
+		if (node->parent == node->parent->parent->left)
+			return (node->parent->parent->right);
+
+		else if (node->parent == node->parent->parent->right)
+			return (node->parent->parent->left);
+	}
+
+	return (NULL);
+}
