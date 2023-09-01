ff5913ff24d3e8548c79a310825ec83435e89ede
Fadyy22 <fadyalaa441@gmail.com>
2023-09-01 23:43:56 +0300

    get balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -47,11 +47,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	size_t left, right;
 
 	if (tree == NULL)
 		return (0);
-
-	size_t left, right;
 
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
