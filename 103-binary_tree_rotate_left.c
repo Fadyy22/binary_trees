b865a4c85ab113cc6b6b6c7a198aa3c017881d62
Reem-Kamal-Ghoniem <reemkamal111@gmail.com>
2023-09-01 17:12:46 -0700

    rotate left edit betty

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -7,35 +7,35 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-    binary_tree_t *new_tree;
+	binary_tree_t *new_tree;
 
-    if (tree == NULL || tree->right == NULL)
-        return (NULL);
+	if (tree == NULL || tree->right == NULL)
+		return (NULL);
 
-    new_tree = tree->right;
-    tree->right = new_tree->left;
+	new_tree = tree->right;
+	tree->right = new_tree->left;
 
-    if (new_tree->left != NULL)
-        new_tree->left->parent = tree;
+	if (new_tree->left != NULL)
+		new_tree->left->parent = tree;
 
-    new_tree->parent = tree->parent;
+	new_tree->parent = tree->parent;
 
-    if (tree->parent == NULL)
-    {
-        new_tree->parent = NULL;
-    }
-    else if (tree->parent->left == tree)
-    {
-        tree->parent->left = new_tree;
-    }
-    else
-    {
-        tree->parent->right = new_tree;
-    }
+	if (tree->parent == NULL)
+	{
+		new_tree->parent = NULL;
+	}
+	else if (tree->parent->left == tree)
+	{
+		tree->parent->left = new_tree;
+	}
+	else
+	{
+		tree->parent->right = new_tree;
+	}
 
-    new_tree->left = tree;
-    tree->parent = new_tree;
+	new_tree->left = tree;
+	tree->parent = new_tree;
 
-    return (new_tree);
+	return (new_tree);
 }
 
